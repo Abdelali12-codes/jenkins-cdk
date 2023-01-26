de53291f7b6a76ed820988a86ca1ee2531ed91ee
Abdelali12-codes <jadelmoulaa3@gmail.com>
2023-01-26 22:15:51 +0000

    new changes

--- src/main/java/com/myorg/conf.java
+++ src/main/java/com/myorg/conf.java
@@ -13,7 +13,7 @@ public class conf {
     public ArrayList<HashMap<String,Object>> groups = new ArrayList<HashMap<String,Object>>();
     
     // list of users you want to create
-    List<String> users = List.of("user1","user2","user3");
+    List<String> users = List.of("user1","user2","user3","user6","user7");
     
     // list of groups you want to create
     public ArrayList<HashMap<String,Object>> groupsList(){
@@ -24,9 +24,9 @@ public class conf {
         this.iam_group1.put("actions", List.of("s3:*","sagemaker:*","ec2:*"));
         
         // group2
-        /*this.iam_group2.put("groupname","group2");
+        this.iam_group2.put("groupname","group2");
         this.iam_group2.put("users", List.of("user1","user2","user3"));
-        this.iam_group2.put("actions", List.of("iam:*","eks:*"));*/
+        this.iam_group2.put("actions", List.of("iam:*","eks:*"));
         // group3
         this.iam_group3.put("groupname","group3");
         this.iam_group3.put("users", List.of("user1","user2"));
@@ -39,7 +39,7 @@ public class conf {
        
         
         this.groups.add(this.iam_group1);
-        //this.groups.add(this.iam_group2);
+        this.groups.add(this.iam_group2);
         this.groups.add(this.iam_group3);
         this.groups.add(this.iam_group4);
         return this.groups;
